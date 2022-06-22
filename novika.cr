46817a35949c245602798f6454c34f9da48c4e77
homonoidian <homonoidian@yandex.ru>
2022-06-22 14:10:51 +0300

    Remove basis, split into several files
    For that get rid of `use` and implement a basic import system in Crystal

--- novika.cr
+++ novika.cr
@@ -1,5 +1,6 @@
 require "big"
 require "colorize"
+require "file_utils"
 
 require "./tape"
 require "./primitives"
@@ -102,6 +103,8 @@ module Novika
 
     def initialize(@id)
     end
+
+    delegate :starts_with?, to: id
 
     # Converts this word into a `QuotedWord`.
     def quote
@@ -703,20 +706,122 @@ module Novika
   end
 end
 
-# junk todo
+CDIR  = "directory".colorize.blue
+CFILE = "file".colorize.green
 
-world = Novika::World.new
+def help : NoReturn
+  abort <<-END
+  Welcome to Novika, and thanks for trying it out!
+  
+  One or more arguments must be provided for Novika to properly pick
+  up what you're trying to run. For instance:
 
-unless ARGV.size == 1
-  abort "usage: novika file.nk"
+    $ novika     core             example.nk
+                 ----             ----------
+                 a #{CDIR}        a #{CFILE}
+
+  (1) When you provide a #{CDIR}, Novika will run all *.nk files in
+      that directory. First, *.nk files in the directory itself are run,
+      and then that process is repeated in the sub-directories. For any
+      given directory, the main file in that directory is dirname.nk. it
+      is always run first.
+
+  (2) In other words, a #{CDIR} provided in arguments has higher priority
+      than a #{CFILE}. But then, files in those directories have higher priority
+      than sub-directories.
+
+  You can try running the following command:
+
+    $ novika core hello.nk
+  
+  END
 end
 
-source = File.read(ARGV.first)
+record Mod, entry : Path? = nil, files = [] of Path do
+  def add(file)
+    files << file
+  end
+end
 
-toplevel = Novika::Block.new
-Novika::Primitives.inject(into: toplevel)
+# Collects files and directories as stated in `help`, starting
+# at *root*, and saves them in *mods*.
+def collect(mods, root : Path)
+  if File.file?(entry = root / "#{root.stem}.nk")
+    mods[root] = mod = Mod.new(entry)
+  else
+    mods[root] = mod = Mod.new
+  end
 
-stack = Novika::Block.new
-block = Novika::Block.new(toplevel).slurp(source)
-world.conts.add Novika::Continuation.new(block.to(0), stack)
-world.exhaust
+  Dir.glob(root / "*.nk") do |path|
+    path = Path[path]
+    mod.add(path) unless path == entry
+  end
+
+  Dir.glob(root / "/*/") do |path|
+    collect(mods, Path[path])
+  end
+end
+
+def import(recpt : Novika::Block, donor : Novika::Block)
+  donor.ls.each do |name|
+    unless name.is_a?(Novika::Word) && name.starts_with?('_')
+      recpt.at name, donor.at(name)
+    end
+  end
+end
+
+def run(world, toplevel, path : Path)
+  {% unless flag?(:release) %}
+    puts path.colorize.dark_gray
+  {% end %}
+  source = File.read(path)
+  stack = Novika::Block.new
+  block = Novika::Block.new(toplevel).slurp(source)
+  world.conts.add Novika::Continuation.new(block.to(0), stack)
+  world.exhaust
+  import(toplevel, block)
+end
+
+help if ARGV.empty?
+
+cwd = Path[FileUtils.pwd]
+
+dirs = [] of Path
+files = [] of Path
+
+ARGV.each do |arg|
+  case File
+  when .directory?(arg) then dirs << Path[arg]
+  when .file?(arg)      then files << Path[arg]
+  else
+    abort "#{arg.colorize.bold} is neither a file nor a directory avaliable in #{cwd.to_s}"
+  end
+end
+
+mods = {} of Path => Mod
+
+dirs.each do |path|
+  collect(mods, Path[path])
+end
+
+world = Novika::World.new
+prims = Novika::Block.new
+Novika::Primitives.inject(into: prims)
+toplevel = Novika::Block.new(prims)
+
+# Evaluate module entries fisrt, if any.
+mods.each_value.select(&.entry).each do |mod|
+  run(world, toplevel, mod.entry.not_nil!)
+end
+
+# Then evaluate all other files.
+mods.each_value do |mod|
+  mod.files.each do |file|
+    run(world, toplevel, file)
+  end
+end
+
+# Then evalute user's files.
+files.each do |file|
+  run(world, toplevel, file)
+end
