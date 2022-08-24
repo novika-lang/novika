003fac3753b326a1d1ff7be58b68f7fad703709f
Alexey Yurchenko <homonoidian@yandex.ru>
2022-08-24 20:01:38 +0300

    Minor: typos and line length

--- src/novika/package.cr
+++ src/novika/package.cr
@@ -1,29 +1,27 @@
 module Novika
-  # Class-side interface to a Novika package. All package classes
-  # must `extend` this module and implement this interface.
+  # Class-side (`extend`) interface to a Novika package. All
+  # package classes must be compatible with this module.
   module IPackageClass
     # Returns the frontend identifier of this package.
     abstract def id : String
 
-    # Returns a short description for why the user might need
-    # to include this package.
+    # Returns a short description on what this package provides.
     abstract def purpose : String
 
     # Returns whether this package should be enabled automatically.
     abstract def on_by_default? : Bool
   end
 
-  # Instance-side interface to a Novika package. All package
-  # instances must `include` this module and respond to all
-  # methods specified by it.
+  # Instance-side (`include`) interface to a Novika package.
+  # All package instances must be compatible with this module.
   module IPackage
     # Injects the vocabulary of this package into the *target* block.
     abstract def inject(into target : Block)
   end
 
   # A merge of instance-side and class-side interfaces to a
-  # Novika package. Auto-includes `IPackageClass` and
-  # `IPacakge` for you.
+  # Novika package. Auto-includes `IPackageClass` and `IPackage`
+  # for you.
   module Package
     macro included
       include IPackage
@@ -33,8 +31,8 @@ module Novika
 
   # Returns an array of all package classes.
   #
-  # For your package to be a registered package class, it
-  # must include `Package`.
+  # For your package to be a registered package class, it must
+  # include `Package`.
   def self.packages
     {{ Package.includers }}
   end
