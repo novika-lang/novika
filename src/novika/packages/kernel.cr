6e7f684599576851f0b8f86fcced842b9382af6a
Alexey Yurchenko <homonoidian@yandex.ru>
2022-08-25 17:41:03 +0300

    Merge math package back into kernel
    I guess making a math package was a mistake, let's fix it until it's
    too late

--- src/novika/packages/kernel.cr
+++ src/novika/packages/kernel.cr
@@ -251,6 +251,52 @@ module Novika::Packages
         block.at(index.to_i).push(engine)
       end
 
+      target.at("+", "( A B -- S ): leaves the Sum of two decimals.") do |engine|
+        b = engine.stack.drop.assert(engine, Decimal)
+        a = engine.stack.drop.assert(engine, Decimal)
+        engine.stack.add(a + b)
+      end
+
+      target.at("-", "( A B -- D ): leaves the Difference of two decimals.") do |engine|
+        b = engine.stack.drop.assert(engine, Decimal)
+        a = engine.stack.drop.assert(engine, Decimal)
+        engine.stack.add(a - b)
+      end
+
+      target.at("*", "( A B -- P ): leaves the Product of two decimals.") do |engine|
+        b = engine.stack.drop.assert(engine, Decimal)
+        a = engine.stack.drop.assert(engine, Decimal)
+        engine.stack.add(a * b)
+      end
+
+      target.at("/", "( A B -- Q ): leaves the Quotient of two decimals.") do |engine|
+        b = engine.stack.drop.assert(engine, Decimal)
+        a = engine.stack.drop.assert(engine, Decimal)
+        b.die("division by zero") if b.zero?
+        engine.stack.add(a / b)
+      end
+
+      target.at("rem", "( A B -- R ): leaves the Remainder of two decimals.") do |engine|
+        b = engine.stack.drop.assert(engine, Decimal)
+        a = engine.stack.drop.assert(engine, Decimal)
+        b.die("division by zero") if b.zero?
+        engine.stack.add(a % b)
+      end
+
+      target.at("round", "( D -- Dr ): leaves round Decimal.") do |engine|
+        decimal = engine.stack.drop.assert(engine, Decimal)
+        decimal.round.push(engine)
+      end
+
+      target.at("trunc", "( D -- Dt ): leaves truncated Decimal.") do |engine|
+        decimal = engine.stack.drop.assert(engine, Decimal)
+        decimal.trunc.push(engine)
+      end
+
+      target.at("rand", "( -- Rd ): random decimal between 0 and 1.") do |engine|
+        Decimal.new(rand).push(engine)
+      end
+
       target.at("charCount", "( Q -- N ): leaves N, the amount of characters in Quote") do |engine|
         quote = engine.stack.drop.assert(engine, Quote)
         Decimal.new(quote.string.size).push(engine)
